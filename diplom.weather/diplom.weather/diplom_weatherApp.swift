ce5237b081491c9c16513995add9c957892bc22b
1skyter <rodiontrach78@icloud.com>
2023-03-14 12:11:19 +0100

    weather-1
    prepared project

---
+++ diplom.weather/diplom.weather/diplom_weatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  diplom_weatherApp.swift
+//  diplom.weather
+//
+//  Created by Rodion Trach on 14.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct diplom_weatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
